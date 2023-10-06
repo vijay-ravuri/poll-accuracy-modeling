f9c6b5f6ae6daae48c601105f6b276acbdafafbb
Vijay Ravuri <vijay_ravuri@outlook.com>
2023-10-06 12:16:24 -0700

    extended feature extraction

--- code/feature_extraction.py
+++ code/feature_extraction.py
@@ -1,7 +1,9 @@
 import re
-import spacy # Spacy is a robust NLP library that lets us use their pre-trained models
+# Spacy is a robust NLP library that provides pre-trained models.
+from spacy import load as load_spacy 
+import numpy as np
 
-nlp = spacy.load('en_core_web_trf') # We chose this model for its higher NER performance
+nlp = load_spacy('en_core_web_trf') # We chose this model for its higher NER performance
 
 def person_flag(list): # Used internally for person_finder
     for tup in list:
@@ -42,3 +44,17 @@ def state_agg(state_code): # Records a few locations to their respective state
         return map[state_code]
     else:
         return state_code
+    
+def imputed_checker(text): # used for imputed_600 column
+    if text is not np.nan:
+        return 1 if 'sample size unavailable' in text else 0
+    return 0
+
+def sample_size(num): # for consistency's sake for imputed_600
+    if num == 600:
+        return 1
+    else:
+        return 0
+    
+def imputed_600(df): # returns 1 for columns that were imputed with samplesize = 600
+    return df['samplesize'].apply(sample_size) * df['comment'].apply(imputed_checker)
